739310b81b11872302570b52b2d137b6c40c6247
neel0507 <neelsoni1994@gmail.com>
2018-06-11 18:51:31 +0100

    "Deleted two files"

--- src/Handler/HomePageR.hs
+++
@@ -1,6 +0,0 @@
-module Handler.HomePageR where
-
-import Import
-
-getHomePageRR :: Handler Html
-getHomePageRR = error "Not yet implemented: getHomePageRR"
--- src/Handler/HomeR.hs
+++
@@ -1,6 +0,0 @@
-module Handler.HomeR where
-
-import Import
-
-getHomeRR :: Handler Html
-getHomeRR = error "Not yet implemented: getHomeRR"
