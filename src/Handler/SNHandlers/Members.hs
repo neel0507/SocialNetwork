1adcf0a5f2b7204c9c96e8875d15ef7597d41f13
neel0507 <neelsoni1994@gmail.com>
2018-06-25 22:51:12 +0100

    Created draft members page

--- src/Handler/SNHandlers/Members.hs
+++ src/Handler/SNHandlers/Members.hs
@@ -2,12 +2,29 @@
 
 module Handler.SNHandlers.Members where
 
+import Prelude
 import Import
+import Database.Persist.Sql
 
 getMembersR :: Handler Html
-getMembersR = do
+getMembersR = do    
+    user <- lookupGetParam "view"
+    allUsers <- runDB $ getAllUsers
     defaultLayout $ do
-       $(widgetFile "SNTemplates/members")
+                addScriptRemote "http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"
+                $(widgetFile "SNTemplates/members")
+                toWidget $ case user of
+                         Nothing ->                                                         
+                                   [hamlet|
+                                     <div class="message"><h2>Other Members</h2>
+                                     <ul class="memberlist">
+                                         $forall (Entity userId users) <- allUsers
+                                            <li><a href="@{MembersR}?view=#{userIdent users}">#{userIdent users}</a> <a href="@{MembersR}?add=#{userIdent users}" class="membersListItem" id="member#{fromSqlKey $ userId}">follow</a><a href="@{MembersR}?remove=#{userIdent users}" class="membersListItem" id="member#{fromSqlKey $ userId}" name="removeMember#{fromSqlKey $ userId}">drop</a>
+                                   |]
+                         Just uname ->                
+                                   [hamlet|
+                                      <div class="message"><p>#{uname}&#39;s Profile</p>
+                                   |]
 
-postMembersR :: Handler Html
-postMembersR = error "Not yet implemented: postMembersR"
+getAllUsers :: DB [Entity User]
+getAllUsers = selectList [] [Asc UserId]
