d722dcf9119085694c20cd6a3624c84ff44b9cf3
neel0507 <neelsoni1994@gmail.com>
2018-06-30 23:59:53 +0100

    Members page only shows other members

---
+++ src/Database/Esqueleto/PostgreSQL.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE OverloadedStrings
+ #-}
+-- | This module contain PostgreSQL-specific functions.
+--
+-- /Since: 2.2.8/
+module Database.Esqueleto.PostgreSQL
+  ( arrayAgg
+  , stringAgg
+  , chr
+  ) where
+
+import Database.Esqueleto.Internal.Language
+import Database.Esqueleto.Internal.Sql
+
+
+-- | (@array_agg@) Concatenate input values, including @NULL@s,
+-- into an array.
+--
+-- /Since: 2.2.8/
+arrayAgg :: SqlExpr (Value a) -> SqlExpr (Value [a])
+arrayAgg = unsafeSqlFunction "array_agg"
+
+
+-- | (@string_agg@) Concatenate input values separated by a
+-- delimiter.
+--
+-- /Since: 2.2.8/
+stringAgg
+  :: SqlString s
+  => SqlExpr (Value s) -- ^ Input values.
+  -> SqlExpr (Value s) -- ^ Delimiter.
+  -> SqlExpr (Value s) -- ^ Concatenation.
+stringAgg expr delim = unsafeSqlFunction "string_agg" (expr, delim)
+
+
+-- | (@chr@) Translate the given integer to a character. (Note the result will
+-- depend on the character set of your database.)
+--
+-- /Since: 2.2.11/
+chr :: SqlString s => SqlExpr (Value Int) -> SqlExpr (Value s)
+chr = unsafeSqlFunction "chr"
--- src/Handler/SNHandlers/Members.hs
+++ src/Handler/SNHandlers/Members.hs
@@ -5,11 +5,22 @@ module Handler.SNHandlers.Members where
 import Prelude
 import Import
 import Database.Persist.Sql
+import qualified Database.Esqueleto      as E
+import           Database.Esqueleto      ((^.))
 
 getMembersR :: Handler Html
 getMembersR = do    
     user <- lookupGetParam "view"
-    allUsers <- runDB $ getAllUsers
+    members <- runDB
+             $ E.select
+             $ E.from $ \(member `E.InnerJoin` user) -> do
+                  E.on $ member ^. MemberUserId E.==. user ^. UserId
+                  E.where_ $ member ^. MemberLoggedInUser E.==. E.val False
+                  return
+                      ( member ^. MemberUserId
+                      , user   ^. UserIdent
+                      ) 
+
     defaultLayout $ do
                 addScriptRemote "http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"
                 $(widgetFile "SNTemplates/members")
@@ -18,13 +29,11 @@ getMembersR = do
                                    [hamlet|
                                      <div class="message"><h2>Other Members</h2>
                                      <ul class="memberlist">
-                                         $forall (Entity userId users) <- allUsers
-                                            <li><a href="@{MembersR}?view=#{userIdent users}">#{userIdent users}</a> <a href="@{MembersR}?add=#{userIdent users}" class="membersListItem" id="member#{fromSqlKey $ userId}">follow</a><a href="@{MembersR}?remove=#{userIdent users}" class="membersListItem" id="member#{fromSqlKey $ userId}" name="removeMember#{fromSqlKey $ userId}">drop</a>
+                                         $forall (E.Value userId, E.Value userIdent) <- members
+                                            <li><a href="@{MembersR}?view=#{userIdent}">#{userIdent}</a> <a href="@{MembersR}?add=#{userIdent}" class="membersListItem" id="member#{fromSqlKey $ userId}">follow</a><a href="@{MembersR}?remove=#{userIdent}" class="membersListItem" id="member#{fromSqlKey $ userId}" name="removeMember#{fromSqlKey $ userId}">drop</a>
                                    |]
                          Just uname ->                
                                    [hamlet|
                                       <div class="message"><p>#{uname}&#39;s Profile</p>
                                    |]
 
-getAllUsers :: DB [Entity User]
-getAllUsers = selectList [] [Asc UserId]
